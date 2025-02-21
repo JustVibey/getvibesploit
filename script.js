2923d1787fc6d870ff2ed3abbcc810ef77d63bea
JustVibey <97280054+JustVibey@users.noreply.github.com>
2025-02-21 18:45:30 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    const x = e.clientX;
-    const y = e.clientY;
-
-    document.body.style.setProperty('--cursorX', x + 'px');
-    document.body.style.setProperty('--cursorY', y + 'px');
-});
